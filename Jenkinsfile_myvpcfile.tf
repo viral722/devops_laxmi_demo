8e06ce483a892b49155d9b7aa1034130f9f4267f
viral722 <viralpanchal13@gmail.com>
2024-10-11 15:14:38 +0530

    Update Jenkinsfile_myvpcfile.tf

--- Jenkinsfile_myvpcfile.tf
+++ Jenkinsfile_myvpcfile.tf
@@ -6,13 +6,11 @@ terraform {
     }
   }
 }
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
  access_key = "AKIAYK5VJLLJ2PVJJY6X"
   secret_key = "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"
 }
-# Create a VPC
   resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
